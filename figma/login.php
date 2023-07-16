a87e49a044a300333eb15d497967e31bb915c368
Maayan <maayanba24@gmail.com>
2023-07-17 02:28:39 +0300

    five

--- figma/login.php
+++ figma/login.php
@@ -1,76 +1,63 @@
 <?php
 include "config.php";
+
+session_start();
+
+if (!empty($_POST["loginMail"])){
+    $query = "SELECT * FROM tbl_227_users WHERE email='"
+        . $_POST["loginMail"]
+        . "' and password = '"
+        . $_POST["loginPass"]
+        . "'";
+
+    $result = mysqli_query($connection, $query);
+    $row = mysqli_fetch_array($result);
+
+    if (is_array($row)) {
+        $_SESSION["id"] = $row['id'];
+        $_SESSION["user_type"] = $row['user_type'];
+        header('Location: ' . URL . 'index.php');
+    } else 
+        $message = "user not found. try again!";
+}
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Login Page</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css%22%3E">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="./css/style.css">
+  <title>Login</title>
 </head>
 <body>
-    <?php
-    // Check if the form is submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        // Database connection details
-        $servername = "your_servername";
-        $username = "your_username";
-        $password = "your_password";
-        $dbname = "your_database_name";
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        // Fetch username and password from the login form
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        // Prepare and execute the SQL query
-        $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        // Check if any user with the given username exists in the database
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $hashed_password = $row["password"];
-
-                // Verify the password
-                if (password_verify($password, $hashed_password)) {
-                    // Start a session and store the user data
-                    session_start();
-                    $_SESSION["username"] = $row["username"];
-                    $_SESSION["user_id"] = $row["id"];
-
-                    //
-
-
-                // Redirect to the main page of the system
-                header("Location: main_page.php");
-                exit();
-            } else {
-                echo "Invalid username or password.";
-            }
-        } else {
-            echo "Invalid username or password.";
-        }
-
-        // Close the database connection
-        $conn->close();
-    }
-    ?>
+<a href="index.html" id="logo">Logo</a>
+  <div class="redheart">
+    <p><b><span class="red">Red</span><span class="heart">Heart</span></b></p>
+  </div>
     <h2>Login</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
-        <label for="password">Password:</label>
-        <input type="password" name="password" required><br>
+    <form novalidate class="needs-validation" method="post" action="index.php">
+        email: <input type="email" name="mail" required><br>
+        <label for="password">Password: </label>
+        <input type="password" name="pass" pattern="[0-9a-zA-Z]+" minlength="6" maxlength="20" required><br>
+        <p>must be 6-20 characters long.</p>
         <input type="submit" value="Login">
     </form>
 </body>
-</html>
+</html> 
+
+<?php
+mysqli_close($connection);
+?>
--- figma/tempfigma.php
+++ figma/tempfigma.php
@@ -12,19 +12,19 @@ include "config.php";
 </head>
 <body>
     <?php
-
-            $nurse = $_GET['nurse'];
-            $name = $_GET['name'];
-            $need = $_GET['need'];
-            $kind = $_GET['kind'];
-            $sum = $_GET['sum'];
-            $until = $_GET['date'];
-            echo "<p>שם האחות: $nurse</p>";
-            echo "<p>שם הקמפיין: $name</p>";
-            echo "<p>צורך: $need</p>";
-            echo "<p>סוג דם: $kind</p>";
-            echo "<p>כמות נדרשת: $sum</p>";
-            echo "<p>תאריך יעד: $until</p>";
+        
+            // $nurse = $_GET['nurse'];
+            // $name = $_GET['name'];
+            // $need = $_GET['need'];
+            // $kind = $_GET['kind'];
+            // $sum = $_GET['sum'];
+            // $until = $_GET['date'];
+            // echo "<p>שם האחות: $nurse</p>";
+            // echo "<p>שם הקמפיין: $name</p>";
+            // echo "<p>צורך: $need</p>";
+            // echo "<p>סוג דם: $kind</p>";
+            // echo "<p>כמות נדרשת: $sum</p>";
+            // echo "<p>תאריך יעד: $until</p>";
         ?>
 </body>
 </html>
